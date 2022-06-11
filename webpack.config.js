e122042b0d80c6e203c51e3859cdacabe4eff999
taiyuHonma <31509007+hmwri@users.noreply.github.com>
2022-06-11 15:25:48 +0900

    環境構築

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
+
+module.exports = {
+  // development or production。
+    mode: 'development',
+      resolve: {
+    extensions: ['.ts', '.js'],
+  },
+    module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: 'ts-loader'}
+        ]
+      }
+    ]
+  },
+  // エントリーポイントの設定
+  entry: './src/index.ts',
+  // 出力の設定
+  output: {
+    // 出力するファイル名
+    filename: 'index.js',
+    // 出力先のパス
+    path: path.join(__dirname, './public/lib')
+    },
+  
+};
